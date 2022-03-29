1f9f9f4ebec6d172abb8179464f964aa81ccbfad
Jaehyeonee <92504386+Jaehyeonee@users.noreply.github.com>
2022-03-29 13:16:13 +0900

    Update 0324_2.cpp

--- graphic0324_2/0324_2.cpp
+++ graphic0324_2/0324_2.cpp
@@ -29,7 +29,7 @@ int pick(int, int);
 
 /* globals */
 
-GLsizei wh = 500, ww = 500; /* initial window size */
+GLsizei wh = 500, ww = 500; /* initial window size */		
 GLfloat size = 3.0;   /* half side length of square */
 int draw_mode = 0; /* drawing mode */
 int rx, ry; /*raster position*/
@@ -106,10 +106,10 @@ void mymouse(int btn, int state, int x, int y)
 		glPushAttrib(GL_ALL_ATTRIB_BITS);
 		where = pick(x, y);
 		glColor3f(r, g, b);
-		if (where != 0)
-		{
-			count = 0;
-			draw_mode = where;
+		if (where != 0)					// where=0이면 그림을 그리겠다 , 0이 아니면 메뉴가 선택되었다--> 새로운 그림이 그려져야 함
+		{								// where 아래 0 
+			count = 0;					// 초기화
+			draw_mode = where;			// draw_mode setting
 		}
 		else switch (draw_mode)
 		{
@@ -191,29 +191,29 @@ void mymouse(int btn, int state, int x, int y)
 		}
 		}   /* endelse*/
 	}/*endif*/
-	glPopAttrib();
-	glFlush();
+	glPopAttrib();								// 메뉴를 선택한 후에는 그림을 그려지는게 아니고 mode만 세팅 됨 따라서 마우스를 한번더 클릭해야됨 그러면 my mouse 다시 또 어딜 클릭했는지 호출됨 
+	glFlush();								 	// 
 }
 
-int pick(int x, int y)
+int pick(int x, int y)							// 클릭시 모니터의 클릭 위치 값이 들어옴
 {
-	y = wh - y;
+	y = wh - y;									// y 값을 gl의 값으로 변경 ???
 	if (y < wh - ww / 10) return 0;
-	else if (x < ww / 10) return LINE;
+	else if (x < ww / 10) return LINE;			// 메뉴의 영역을 나눔 
 	else if (x < ww / 5) return RECTANGLE;
 	else if (x < 3 * ww / 10) return TRIANGLE;
 	else if (x < 2 * ww / 5) return PPOINTSS;
 	else if (x < ww / 2) return TEXTT;
-	else return 0;
+	else return 0;								// 전체에서 메뉴의 사각형을 뺀 나머지 부분은 다 display 영역
 }
 
 void screen_box(int x, int y, int s)
 {
 	glBegin(GL_QUADS);
-	glVertex2i(x, y);
-	glVertex2i(x + s, y);
-	glVertex2i(x + s, y + s);
-	glVertex2i(x, y + s);
+	glVertex2i(x, y);					// 좌 하향 
+	glVertex2i(x + s, y);				// 우 하향
+	glVertex2i(x + s, y + s);			// 우 상향
+	glVertex2i(x, y + s);				// 좌 상향 
 	glEnd();
 }
 
@@ -230,7 +230,7 @@ void right_menu(int id)
 
 void middle_menu(int id)
 {
-
+	// middle 버튼을 누르면 mymouse를 호출하지만 mymouse안에 middle에 대한 게 없으니가
 }
 
 void color_menu(int id)
@@ -273,15 +273,15 @@ void mydisplay(void)
 	int shift = 0;
 
 	glColor3f(1.0, 1.0, 1.0);
-	screen_box(0, wh - ww / 10, ww / 10);
+	screen_box(0, wh - ww / 10, ww / 10);					// line box: window size 바뀔 때마다 reshape (x= 40, size 50)
 	glColor3f(1.0, 0.0, 0.0);
-	screen_box(ww / 10, wh - ww / 10, ww / 10);
+	screen_box(ww / 10, wh - ww / 10, ww / 10);				// rectangle box : 
 	glColor3f(0.0, 1.0, 0.0);
-	screen_box(ww / 5, wh - ww / 10, ww / 10);
+	screen_box(ww / 5, wh - ww / 10, ww / 10);				// triangle box
 	glColor3f(0.0, 0.0, 1.0);
-	screen_box(3 * ww / 10, wh - ww / 10, ww / 10);
+	screen_box(3 * ww / 10, wh - ww / 10, ww / 10);			// point	
 	glColor3f(1.0, 1.0, 0.0);
-	screen_box(2 * ww / 5, wh - ww / 10, ww / 10);
+	screen_box(2 * ww / 5, wh - ww / 10, ww / 10);			// string
 	glColor3f(0.0, 0.0, 0.0);
 	glBegin(GL_LINES);
 	glVertex2i(wh / 40, wh - ww / 20);
@@ -296,16 +296,17 @@ void mydisplay(void)
 	glVertex2i(ww / 5 + 3 * ww / 40, wh - ww / 10 + ww / 40);
 	glEnd();
 	glPointSize(3.0);
+
 	glBegin(GL_POINTS);
 	glVertex2i(3 * ww / 10 + ww / 20, wh - ww / 20);
 	glEnd();
-	glRasterPos2i(2 * ww / 5, wh - ww / 20);
-	glutBitmapCharacter(GLUT_BITMAP_9_BY_15, 'A');
-	shift = glutBitmapWidth(GLUT_BITMAP_9_BY_15, 'A');
-	glRasterPos2i(2 * ww / 5 + shift, wh - ww / 20);
-	glutBitmapCharacter(GLUT_BITMAP_9_BY_15, 'B');
-	shift += glutBitmapWidth(GLUT_BITMAP_9_BY_15, 'B');
-	glRasterPos2i(2 * ww / 5 + shift, wh - ww / 20);
+	glRasterPos2i(2 * ww / 5, wh - ww / 20);				// 위치 지정 glRasterPos2i() , 
+	glutBitmapCharacter(GLUT_BITMAP_9_BY_15, 'A');			// 글씨(이미지)가 출력됨 glutBitmapCharacter '글씨 사이즈를 9by15 크기의 bitmap으로'
+	shift = glutBitmapWidth(GLUT_BITMAP_9_BY_15, 'A');		// bitmap의 너비를 구해서 shift변수에 넣어라 glutBitmapWidth()	
+	glRasterPos2i(2 * ww / 5 + shift, wh - ww / 20);		// A를 썼던 위치를 밀어서 너비만큼 옮기고 그 위치에 B를 써라
+	glutBitmapCharacter(GLUT_BITMAP_9_BY_15, 'B');			
+	shift += glutBitmapWidth(GLUT_BITMAP_9_BY_15, 'B');		// width를 구해서 shift에 더하기 
+	glRasterPos2i(2 * ww / 5 + shift, wh - ww / 20);	
 	glutBitmapCharacter(GLUT_BITMAP_9_BY_15, 'C');
 	glFlush();
 
@@ -319,7 +320,7 @@ int main(int argc, char** argv)
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutCreateWindow("square");
-	c_menu = glutCreateMenu(color_menu);
+	c_menu = glutCreateMenu(color_menu);			//선택한 값이 c_menu에 값을 전달 
 	glutAddMenuEntry("Red", 1);
 	glutAddMenuEntry("Green", 2);
 	glutAddMenuEntry("Blue", 3);
@@ -334,20 +335,20 @@ int main(int argc, char** argv)
 	f_menu = glutCreateMenu(fill_menu);
 	glutAddMenuEntry("fill on", 1);
 	glutAddMenuEntry("fill off", 2);
-	glutCreateMenu(right_menu);
+	glutCreateMenu(right_menu);					// right menu
 	glutAddMenuEntry("quit", 1);
 	glutAddMenuEntry("clear", 2);
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
-	glutCreateMenu(middle_menu);
+	glutCreateMenu(middle_menu);				// middle menu 
 	glutAddSubMenu("Colors", c_menu);
 	glutAddSubMenu("Pixel Size", p_menu);
 	glutAddSubMenu("Fill", f_menu);
 	glutAttachMenu(GLUT_MIDDLE_BUTTON);
 	myinit();
-	glutReshapeFunc(myReshape);
+	glutReshapeFunc(myReshape);					// 함수 저장 
 	glutKeyboardFunc(key);
-	glutMouseFunc(mymouse);
-	glutDisplayFunc(mydisplay);
-	glutMainLoop();
+	glutMouseFunc(mymouse);						// mouse에 대한 d이벤트가 생기면 mymouse를 무조건 호출함
+	glutDisplayFunc(mydisplay);					// 제일 처음으로 
+	glutMainLoop();								// mydisplay 함수로 감 
 	return 0;
 }
